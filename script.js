23418af13606c8a0ec389ce59d83949e6df24cc7
Laise France <134656901+laisefarias@users.noreply.github.com>
2023-07-11 02:21:09 -0300

    add table

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
-// credenciais da API
-const domain = 'b24-4qk6uy.bitrix24.com.br'
-const accessToken = 's0mujvynyef1p9vh'
-
-// Obter perfil
-axios
-  .get(`https://${domain}/rest/1/${accessToken}/profile.json`)
-  .then((response) => {
-    console.log(response.data.result)
-  })
-  .catch((error) => {
-    console.log(error.response.data)
-  })
+// Credenciais da API
+const domain = 'b24-4qk6uy.bitrix24.com.br';
+const accessToken = 's0mujvynyef1p9vh';
 
 // Função para carregar os departamentos do Bitrix24
 function carregarDepartamentos() {
-  const url =
-    'https://b24-4qk6uy.bitrix24.com.br/rest/1/wkve83h3dtso2txe/department.get.json'
+  const url = `https://${domain}/rest/1/wkve83h3dtso2txe/department.get.json`;
 
   axios
     .get(url)
     .then((response) => {
-      const selectDepartments = document.querySelector('#selectDepartments')
+      const selectDepartments = document.querySelector('#selectDepartments');
 
       response.data.result.forEach((department) => {
-        const option = document.createElement('option')
-        option.value = department.ID
-        option.textContent = department.NAME
+        const option = document.createElement('option');
+        option.value = department.ID;
+        option.textContent = department.NAME;
+        option.setAttribute('data-department', department.ID);
+        option.setAttribute('data-department-name', department.NAME);
+        option.setAttribute('data-department-parent', department.PARENT);
+        option.setAttribute('data-department-sort', department.SORT);
 
-        selectDepartments.appendChild(option)
-      })
+        selectDepartments.appendChild(option);
+      });
     })
     .catch((error) => {
-      console.error(error)
+      console.error(error);
+    });
+}
+
+// Função para carregar os colaboradores do Bitrix24
+function carregarColaboradores() {
+  const url = `https://${domain}/rest/1/wkve83h3dtso2txe/user.get.json`;
+
+  axios
+    .get(url)
+    .then((response) => {
+      response.data.result.forEach((colaborador) => {
+        const tbody = document.querySelector('#tabelaBitrix24 tbody');
+
+        const row = document.createElement('tr');
+        const funcionarioCell = document.createElement('td');
+        funcionarioCell.textContent = colaborador.NAME;
+        row.appendChild(funcionarioCell);
+
+        const taskIdCell = document.createElement('td');
+        taskIdCell.textContent = colaborador.ID;
+        row.appendChild(taskIdCell);
+
+        const tempoEstimadoCell = document.createElement('td');
+        tempoEstimadoCell.textContent = colaborador.DURATION;
+        row.appendChild(tempoEstimadoCell);
+
+        tbody.appendChild(row);
+      });
     })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 // Chama a função para carregar os departamentos ao carregar a página
-document.addEventListener('DOMContentLoaded', carregarDepartamentos)
+document.addEventListener('DOMContentLoaded', () => {
+  carregarDepartamentos();
+  carregarColaboradores();
+});
